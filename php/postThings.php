be543ed30c2f19786a2e22d44cbd4bdf9be7a2a2
Manuell00 <manuell.caselli@gmail.com>
2023-06-22 16:55:00 +0200

    Inserimento rimozione duplicaati

--- php/postThings.php
+++ php/postThings.php
@@ -16,19 +16,31 @@ $newThing = $_POST;
 // Leggo i dati dal file JSON come stringa
 $dataStr = file_get_contents($file);
 
-// Decodifico la stringa JSON in una variabile
-$data = json_decode($dataStr);
+// Decodifico la stringa JSON in un array associativo
+$data = json_decode($dataStr, true);
 
-// Aggiungo i nuovi dati alla variabile $data
-$data[] = $newThing;
+// Verifico se il nuovo dato è già presente nell'array
+$isDuplicate = false;
+foreach ($data as $item) {
+  if ($item == $newThing) {
+    $isDuplicate = true;
+    break;
+  }
+}
 
-// Codifico i dati in formato JSON
-$encData = json_encode($data);
+// Se il dato è duplicato, non aggiungere nulla
+if ($isDuplicate === false) {
+    // Aggiungo i nuovi dati alla variabile $data
+    $data[] = $newThing;
+}
 
-// Scrivo i dati nel file JSON
-file_put_contents($file, $encData);
+  // Codifico i dati in formato JSON
+  $encData = json_encode($data);
 
-// Restituisco i dati codificati come risposta
-echo $encData;
+  // Scrivo i dati nel file JSON
+  file_put_contents($file, $encData);
+
+  // Restituisco i dati codificati come risposta
+  echo $encData;
 
 ?>
